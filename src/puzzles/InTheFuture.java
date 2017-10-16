a57ab14d611ef9f584cb475040ace37f3ef9d61f
Vinay <Vaddanki@github.com>
2017-10-16 19:39:04 +0530

    Added In the Future puzzles sol

--- src/puzzles/InTheFuture.java
+++ src/puzzles/InTheFuture.java
@@ -1,10 +1,39 @@
 package puzzles;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class InTheFuture {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws IOException {
 
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Asha solved problems:");
+		int A = sc.nextInt();
+		System.out.println("Kelly solved problems:");
+		int K = sc.nextInt();
+		System.out.println("No of problems ahead:");
+		int P = sc.nextInt();
+		System.out.println("Output: " + minNum(A, K, P));
 	}
 
+	static int minNum(int A, int K, int P) {
+
+		int count = 0, a = A, k = K;
+		boolean condition = true;
+		while (condition) {
+			if (count == 0)
+				a = A + P;
+			count++;
+			if (k > a) {
+				condition = false;
+				break;
+			} else if (count >= 1) {
+				k = k + K;
+				a = a + A;
+			}
+		}
+		return count;
+	}
 }
